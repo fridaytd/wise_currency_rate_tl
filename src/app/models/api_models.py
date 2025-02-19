12c6f998c1e9cf8b5cde147b00da53d29045d63b
thdangtr <thdangtr@gmail.com>
2025-02-19 23:04:31 +0700

    fix timezone bug

--- src/app/models/api_models.py
+++ src/app/models/api_models.py
@@ -14,6 +14,6 @@ class CurrencyRate(BaseModel):
         self,
     ) -> str:
         time = datetime.fromisoformat(self.time)
-        vn_timezone_time = time.astimezone(timezone("Etc/GMT+7"))
+        vn_timezone_time = time.astimezone(timezone("Asia/Ho_Chi_Minh"))
         formatted_date = vn_timezone_time.strftime("%d/%m/%Y %H:%M:%S")
         return formatted_date
